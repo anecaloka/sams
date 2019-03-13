877054bb37ba716e2401f4cdf64cf6c26a406205
aneca_loka <anec@uni-kassel.de>
2019-03-13 22:15:31 +0100

    added GPIO.cleanup

--- RelayControl.py
+++ RelayControl.py
@@ -21,7 +21,7 @@ GPIO.setmode(GPIO.BCM)
 
 # init list with GPIO pin numbers as they are connected to the RPi
 pinList= [5, 12, 6, 7, 13, 25, 19, 24, 26, 23, 21, 18, 20, 15, 16]
-pinGSM = 22
+pinGSM = 27
 
 # The array portsActive is set by the user control surface and
 # saved on the local storage so it will not get lost when reboot.
